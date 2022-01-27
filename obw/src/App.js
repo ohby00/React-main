7cc941029721e73823bd450a2d58d1aecbf34939
ohby00 <quddnr217@naver.com>
2022-01-27 14:53:22 +0900

    upload

--- obw/src/App.js
+++ obw/src/App.js
@@ -7,10 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          gggg.
         </p>
         <a
-          className="App-link"
+          className="hdd"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
